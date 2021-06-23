fba585f7bddf5f3f9bf4fd1a127b6062c6940058
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-23 14:51:31 +0530

    Added: Code for Player Position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -3,5 +3,7 @@ public class SnakeandLadder
 public static void main(String[] args)
 {
 System.out.println("Welcome to Snake And Ladder");
+int PlayerPosition=0;
+System.out.println("Player position is : "+PlayerPosition);
 }
 }
