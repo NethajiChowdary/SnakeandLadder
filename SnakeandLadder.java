eee02a3ad811d8dd796fa4ace35ab1d8c2081421
NethajiChowdary <nethajichowdary555@gmail.com>
2021-06-23 16:44:46 +0530

    Modified: Code to check whether it is Ladder or Snake or Noplay

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -7,5 +7,23 @@ int PlayerPosition=1;
 System.out.println("Player position is : "+PlayerPosition);
 int die = (int) (Math.floor( Math.random() * 10) % 6+1);
 System.out.println("Die rolled is :" +die);
+int option =(int) (Math.floor(Math.random() * 10)%3);
+switch(option)
+{
+case 0:
+System.out.println("No play");
+PlayerPosition += 0;
+break;
+case 1:
+System.out.println("Ladder");
+PlayerPosition += die;
+break;
+default:
+System.out.println("Snake");
+PlayerPosition -= die;
+if(PlayerPosition<0)
+PlayerPosition=0;
+}
+System.out.println("Player position is : "+PlayerPosition);
 }
 }
